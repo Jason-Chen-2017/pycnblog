5172f2f2deea764d891c69dfd14efd6d10169e82
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2023-12-01 00:24:55 +0800

    2023年 12月 01日 星期五 00:24:55 CST

--- git_commit.sh
+++ git_commit.sh
@@ -9,6 +9,6 @@ git add *
 # 提交更改到仓库
 git commit -m "$msg"
 # https://github.com/settings/tokens
-git remote set-url origin https://Jason-Chen-2017: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@github.com/Jason-Chen-2017/pycnblog.git
+git remote set-url origin https://Jason-Chen-2017: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@github.com/Jason-Chen-2017/pycnblog.git
 
 git push origin master
