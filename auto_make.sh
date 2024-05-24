a0c7bbf7f8b60e036e1e71e77197ab0a9f2c75c6
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2024-05-24 20:46:07 +0800

    auto_make.sh find_good_articles.py

--- auto_make.sh
+++ auto_make.sh
@@ -4,9 +4,9 @@ while true
 do
   echo "执行任务：auto make 时间：$(date +%Y-%m-%d_%H:%M:%S)"
   make m
-  sleep 60
+  sleep 30
   make f
-  sleep 10
-  make g
+  sleep 30
+  make gu
   sleep 3600
 done
--- find_good_articles.py
+++ find_good_articles.py
@@ -25,7 +25,7 @@ def is_good_content(content):
 
     # 包含关键字：$$ 表示有公式，```表示有代码
     keywords = [
-        "$",
+         "$",
         "```",
         "背景介绍",
         "核心概念与联系",
@@ -33,18 +33,19 @@ def is_good_content(content):
         "数学模型和公式",
         "项目实践",
         "实际应用场景",
-        # "工具和资源",
     ]
 
     keywords2 = [
         "$",
         "```",
         "Background Introduction",
-        "Core Concepts",
-        "Core Algorithms",
-        "Best Practices",
-        "Real-World Applications",
+        "Core Concept",
+        "Core Algorithm",
+        "Mathematical Model",
+        "Project Implementation",
+        "Practical Application",
         # "Tools and Resources",
+        # "Future Development Trends and Challenges",
     ]
 
     flag1 = True
