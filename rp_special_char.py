617de39bba9545f4df54d06428b5edc884bcf0a3
me <chenguangjian.jk@bytedance.com>
2024-07-04 02:55:52 +0800

    2024年 07月 04日 星期四 02:55:51 CST

---
+++ rp_special_char.py
@@ -0,0 +1,51 @@
+import os
+
+import re
+
+
+def process_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # 特殊关键字，不进行替换的部分
+    special_keywords = ["\\\\nebula", "\\\\nabla", "\\\\neq", "\\\\not", "\\\\neg"]
+
+    # 替换 \\n 为 \n，但保留特殊关键字
+
+    def replace_newline(match):
+        if match.group(0) in special_keywords:
+            return match.group(0)
+
+        return match.group(0).replace("\\n", "\n")
+
+    content = re.sub(r'\\\\n|\\\\nebula|\\\\nabla|\\\\neq|\\\\not|\\\\neg', replace_newline, content)
+
+    # 替换 \" 为 "
+    content = content.replace('\\"', '"')
+    # 替换 \\ 为 \， 但是本行末尾的 \\ 不替换
+    content = re.sub(r'\\\\(?!$)', '\\', content)
+    
+    # 替换 \\[ 为 $$
+    content = content.replace('\\[', '$$')
+    # 替换 \\] 为 $$
+    content = content.replace('\\]', '$$')
+
+    with open(filepath, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+
+def process_directory(directory):
+    for root, dirs, files in os.walk(directory):
+
+        for file in files:
+            filepath = os.path.join(root, file)
+
+            process_file(filepath)
+            print(filepath)
+
+
+if __name__ == "__main__":
+    # 替换为你要处理的目录路径
+    directory_path = "/home/me/tools/pycnblog/blog"
+
+    process_directory(directory_path)
