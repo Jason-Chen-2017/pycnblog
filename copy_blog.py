a7b2e3672c9b9443794de68df9dc9f6092fdfa0f
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2024-07-08 02:33:40 +0800

    Mon Jul  8 02:33:36 CST 2024

--- copy_blog.py
+++ copy_blog.py
@@ -52,6 +52,6 @@ def copy_blog(src, n, dst, start):
 
 
 if __name__ == '__main__':
-    src = '/Users/bytedance/ai/pycnblog/articles_good/20240706'
+    src = '/Users/bytedance/ai/pycnblog/articles_good/20240707'
     dst = '/Users/bytedance/ai/pycnblog/blog/zen'
     copy_blog(src, 100, dst, 158)
