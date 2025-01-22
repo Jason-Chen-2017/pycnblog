a983c15309b2c0d1077a8c52c58188e27733e4e7
me <chenguangjian.jk@bytedance.com>
2025-01-23 01:13:10 +0800

    2025年 01月 23日 星期四 01:13:05 CST

--- copy_blog_u.py
+++ copy_blog_u.py
@@ -54,11 +54,11 @@ def copy_blog(src, n, dst, start):
 
 if __name__ == '__main__':
 
-    dst = '/home/me/tools/pycnblog/blog/agent'
+    dst = '/home/me/tools/pycnblog/blog/vi'
 
-    src = '/home/me/tools/pycnblog/articles_good/20250120'
+    src = '/home/me/tools/pycnblog/articles_good/20250122'
 
-    copy_blog(src, 60, dst, 1432)
+    copy_blog(src, 88, dst, 33)
 
     # src = '/Users/bytedance/ai/pycnblog/articles_good/20240929'
     # dst = '/Users/bytedance/ai/pycnblog/blog/agi'
